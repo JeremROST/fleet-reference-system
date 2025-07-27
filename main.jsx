dcc877db7070e6ec6e9e2ba6e80989acd5e600b2
JeremROST <jerem.s@icloud.com>
2025-07-27 16:19:38 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FleetInventoryApp from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FleetInventoryApp />
+  </React.StrictMode>
+);
